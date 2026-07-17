5069f6db8f47277583fc147ea9425f24060def2b
andymdavid <andy@otherstuff.studio>
2026-07-17 08:00:55 +0000

    Fix mobile insight article layout

--- scripts/generate-insights.mjs
+++ scripts/generate-insights.mjs
@@ -220,7 +220,7 @@ ${post.image ? `        "image": "https://offpistestudio.com${escapeHtml(post.im
     ]
   }
   </script>
-  <link rel="stylesheet" href="/src/styles/main.css?v=20260708-mobile-cta-profile-left-v70">
+  <link rel="stylesheet" href="/src/styles/main.css?v=20260717-insight-mobile-byline-v71">
   <link rel="preload" href="/public/fonts/fonnts.com-Alfabet_Regular.otf" as="font" type="font/otf" crossorigin>
   <link rel="preload" href="/public/fonts/fonnts.com-Alfabet_Black.otf" as="font" type="font/otf" crossorigin>
   <link rel="preconnect" href="https://fonts.googleapis.com">
